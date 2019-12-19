767aeec99ba62b40d353105f21a5425915466cac
Kevin Brutus <kevinbrutus@pursuit.org>
2019-12-18 21:27:46 -0500

    add select and form event listener. work on css.

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
     let select = document.querySelector("select")
+    let form = document.querySelector("form")
+    let ul = document.querySelector("ul")
     let info = document.querySelector("#info")
     let userInput = document.querySelector("#text")
     let submit = document.querySelector("#submit")
+
     const fetchFilms = async () => {
-        let res = await axios.get("https://ghibliapi.herokuapp.com/films/")
-        let films = res.data
-        films.forEach(film => {
-            let option = document.createElement("option")
-            option.innerText = film.title
-            select.appendChild(option)
-        })
+        try{
+            let res = await axios.get("https://ghibliapi.herokuapp.com/films/")
+            let data = res.data
+
+            data.forEach(film => {
+                let option = document.createElement("option")
+                option.innerText = film.title
+                select.appendChild(option)
+            })
+
+            select.addEventListener("change", (event) => {
+                info.innerHTML = ""
+                let filmTitle = document.createElement("h3")
+                filmTitle.innerText = event.target.value
+                info.appendChild(filmTitle)
+
+                form.addEventListener("submit", (e) => {
+                    e.preventDefault()
+                    let li = document.createElement("li")
+                    li.innerText = event.target.value + ": " + userInput.value
+                    ul.appendChild(li)
+                    userInput.value = ""
+                })
+            })
+        }
+        catch(err){
+            console.log(err)
+        }
     } 
+    
     fetchFilms()
+
 })
